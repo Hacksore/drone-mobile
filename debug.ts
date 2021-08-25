261f5a5b199aa289f75af0cd58041e6d5cfb4fcc
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-08-25 14:17:41 -0500

    Fix status and location calls (#4)

---
+++ debug.ts
@@ -0,0 +1,104 @@
+import config from './config.json';
+import DroneMoble from './src';
+import inquirer from 'inquirer';
+
+const { username, password } = config;
+
+let client: DroneMoble;
+let vehicleId;
+
+const apiCalls = {
+  exit: { run: () => true },
+  status: {
+    run: async () => {
+      const status = await client.status(vehicleId);
+      console.log('status', JSON.stringify(status, null, 2));
+    },
+  },
+  start: {
+    run: async () => {
+      const start = await client.start(vehicleId);
+      console.log('start', JSON.stringify(start, null, 2));
+    },
+  },
+  stop: {
+    run: async () => {
+      const stop = await client.stop(vehicleId);
+      console.log('stop', JSON.stringify(stop, null, 2));
+    },
+  },
+  lock: {
+    run: async () => {
+      const lock = await client.lock(vehicleId);
+      console.log('lock', JSON.stringify(lock, null, 2));
+    },
+  },
+  unlock: {
+    run: async () => {
+      const unlock = await client.unlock(vehicleId);
+      console.log('unlock', JSON.stringify(unlock, null, 2));
+    },
+  },
+  trunk: {
+    run: async () => {
+      const trunk = await client.trunk(vehicleId);
+      console.log('trunk', JSON.stringify(trunk, null, 2));
+    },
+  },
+  aux1: {
+    run: async () => {
+      const aux1 = await client.aux1(vehicleId);
+      console.log('aux1', JSON.stringify(aux1, null, 2));
+    },
+  },
+  aux2: {
+    run: async () => {
+      const aux2 = await client.aux2(vehicleId);
+      console.log('aux2', JSON.stringify(aux2, null, 2));
+    },
+  },
+  location: {
+    run: async () => {
+      const location = await client.location(vehicleId);
+      console.log('location:', JSON.stringify(location, null, 2));
+    },
+  },
+};
+
+const onReadyHandler = async () => {
+  // get a list of vehicles on the account
+  const vehicleList = await client.vehicles();
+  vehicleId = vehicleList[0]?.device_key;
+  
+  askForCommandInput();
+};
+
+const createInstance = () => {
+  client = new DroneMoble({
+    username,
+    password,
+  });
+  client.on('ready', onReadyHandler);
+};
+
+function askForCommandInput() {
+  console.log('');
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'command',
+        message: 'What you wanna do?',
+        choices: Object.entries(apiCalls).map(([k, v]) => k),
+      },
+    ])
+    .then(answers => {
+      if (answers.command === 'exit') {
+        return;
+      }
+
+      apiCalls[answers.command].run();
+    });
+}
+
+createInstance();
